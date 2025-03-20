b552000897b06ab9e69a8f64dc52857500d53a81
Elian Xavier <45861861+elianxavier@users.noreply.github.com>
2025-03-20 18:19:10 -0300

    Desenvolvendo tela do filme - em progresso

--- components/starvote.tsx
+++ components/starvote.tsx
@@ -3,7 +3,13 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { Text } from "react-native";
 
 export default function StarVote(props: { vote_value: number }) {
-  const renderStars = (vote_value: number) => {};
+  const renderStars = (vote_value: number) => {
+    const stars = [];
+
+    for (let i = 1; i < 5; i++) {
+      stars.push();
+    }
+  };
 
   return (
     <Text>
